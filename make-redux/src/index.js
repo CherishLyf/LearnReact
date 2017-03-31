405eaa0d60a82e9ab2eda81d6e57c62887c8e618
CherishLyf <517209885@qq.com>
2017-03-31 23:01:59 +0800

    优雅地修改共享状态

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+const appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red'
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
+  }
+}
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function renderApp (appState) {
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+
+function renderTitle (title) {
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = title.text
+  titleDOM.style.color = title.color
+}
+
+function renderContent (content) {
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = content.text
+  contentDOM.style.color = content.color
+}
+
+// 负责修改数据
+function dispatch (action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      appState.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      appState.title.color = action.color
+      break
+    default:
+      break
+  }
+}
+
+renderApp(appState)   // 首次渲染页面
+dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》'})    // 修改标题文本
+dispatch({type: 'UPDATE_TITLE_COLOR', color: 'blue'})     // 修改标题颜色
+renderApp(appState)   // 把新的数据渲染到页面
