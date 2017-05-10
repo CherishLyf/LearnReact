cac7b2bcabad0b33bf1a797a17df63a3dc1fd572
CherishLyf <517209885@qq.com>
2017-05-10 17:55:45 +0800

    使用 react-redux 完成评论组件

---
+++ comment-app2/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import CommentApp from './containers/CommentApp'
+import CommentsReducer from './reducers/comments'
+import './index.css'
+
+const store = createStore(CommentsReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <CommentApp />
+  </Provider>,
+  document.getElementById('root')
+);
