20297d920fcf7a2612602f84da3f6cf602dee06a
CherishLyf <517209885@qq.com>
2017-05-11 12:03:40 +0800

    react-router 基本使用 和 URL 参数

---
+++ react-router/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import BasicExample from './BasicExample/BasicExample'
+import ParamsExample from './ParamsExample/ParamsExample'
+import './index.css';
+
+ReactDOM.render(
+  // <ParamsExample />,
+  <ParamsExample />,
+  document.getElementById('root')
+);
