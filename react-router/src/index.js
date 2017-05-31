25d31cef2806d3bbe6180f2b9977281f5ce115a0
CherishLyf <517209885@qq.com>
2017-05-31 18:37:12 +0800

    react-router

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import BasicExample from './BasicExample/BasicExample'
-import ParamsExample from './ParamsExample/ParamsExample'
+// import ParamsExample from './ParamsExample/ParamsExample'
+// import AuthExample from './AuthExample/AuthExample'
+// import CustomLinkExample from './CustomLinkExample/CustomLinkExample'
+import PreventingTransitionsExample from './PreventingTransitionsExample/PreventingTransitionsExample'
 import './index.css';
 
 ReactDOM.render(
   // <ParamsExample />,
-  <ParamsExample />,
+  <PreventingTransitionsExample />,
   document.getElementById('root')
 );
